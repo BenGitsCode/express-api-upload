4df9deb3a4204d73e5b41aea4049956f41a1fe1e
payne-chris-r <06cpayne@gmail.com>
2017-03-06 12:29:19 -0500

    Updates script to add ext and filetype
    Uses mime module to get the MIME type to send to amazon. Additionally,
    we used path.extname to grab the extension of the file we're
    uploading so that we can save it with a proper file extension on AWS.
    Next step: Concerned about overwriting filenames...

--- bin/aws-upload.js
+++ bin/aws-upload.js
@@ -6,17 +6,23 @@ const AWS = require('aws-sdk');
 const s3 = new AWS.S3();
 const fs = require('fs');
 
+const mime = require('mime');
+const path = require('path');
+
 let file = {
   path: process.argv[2],
   title: process.argv[3]
 };
 
+let mimeType = mime.lookup(file.path);
+let ext = path.extname(file.path);
 let stream = fs.createReadStream(file.path);
 
 let params = {
   ACL: 'public-read',
+  ContentType: mimeType,
   Bucket: process.env.AWS_S3_BUCKET_NAME,
-  Key: file.title,
+  Key: `${file.title}${ext}`,
   Body: stream
 };
 
